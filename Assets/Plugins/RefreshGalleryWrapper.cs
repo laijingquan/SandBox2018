d8d0bb24dce8dc5acf8a1d866122de4689330857
laijingquan <351779036@qq.com>
2018-01-21 19:34:04 +0800

    loading Game 场景依赖搭建完毕

---
+++ Assets/Plugins/RefreshGalleryWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class RefreshGalleryWrapper : MonoBehaviour
+{
+    private void RefreshGallery(string path)
+    {
+        AndroidJavaClass class2 = new AndroidJavaClass("com.astricstore.androidutil.AndroidGallery");
+        object[] args = new object[] { path };
+        class2.CallStatic("RefreshGallery", args);
+    }
+
+    private void SetGalleryPath()
+    {
+        string name = "CaptureAndSave";
+        CaptureAndSave save = UnityEngine.Object.FindObjectOfType<CaptureAndSave>();
+        if (save != null)
+        {
+            name = save.gameObject.name;
+        }
+        AndroidJavaClass class2 = new AndroidJavaClass("com.astricstore.androidutil.AndroidGallery");
+        object[] args = new object[] { name };
+        class2.CallStatic("SetGalleryPath", args);
+    }
+
+    private void Start()
+    {
+        this.SetGalleryPath();
+        this.StoragePermissionRequest();
+    }
+
+    private void StoragePermissionRequest()
+    {
+        string name = "CaptureAndSave";
+        CaptureAndSave save = UnityEngine.Object.FindObjectOfType<CaptureAndSave>();
+        if (save != null)
+        {
+            name = save.gameObject.name;
+        }
+        AndroidJavaClass class2 = new AndroidJavaClass("com.astricstore.androidutil.AndroidGallery");
+        object[] args = new object[] { name };
+        class2.CallStatic("CheckForPermission", args);
+    }
+}
+
